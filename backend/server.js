5b68445439841bf0290a77ab46e53bafbef90675
Sobin Siby <sobinsibydigitalmarketer@gmail.com>
2026-03-19 02:22:11 +0530

    Refactor server to use Express framework
    Refactor server setup to use Express and add a simple API endpoint.

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
-require('dotenv').config();
-const app = require('./src/app');
-const connectDB = require('./src/config/db');
+const express = require("express");
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// Connect to Database
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}).catch(err => {
-    console.error('Failed to connect to database', err);
-    process.exit(1);
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
