205201454c8b2dc7c86ebb52b827d97d6b8c4594
Sobin Siby <sobinsibydigitalmarketer@gmail.com>
2026-03-19 16:09:34 +0530

    Delete src directory

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/health', (req, res) => {
-    res.status(200).json({ status: 'OK', timestamp: new Date() });
-});
-
-module.exports = router;
--- src/routes/leadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createLead, getLeads } = require('../controllers/leadController');
-
-router.route('/')
-    .post(createLead)
-    .get(getLeads);
-
-module.exports = router;
